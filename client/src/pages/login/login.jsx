891589ea6085dfe6373143ecc38550aff09b54de
Teerut Srithongdee <>
2023-01-27 15:40:04 +0700

    add login

---
+++ client/src/pages/login/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <form className='tw-fixed tw-top-0 tw-bottom-0 tw-right-0 tw-left-0 tw-flex tw-justify-center tw-items-center'>
+        <button className='btn btn-primary'>
+            Login with Facebook
+        </button>
+    </form>
+  )
+}
