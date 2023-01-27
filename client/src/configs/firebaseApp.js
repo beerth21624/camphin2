c71ee4ec3aff28345deaa89dae61d3f478a6abe2
Teerut Srithongdee <>
2023-01-27 16:05:53 +0700

    add login

--- client/src/configs/firebaseApp.js
+++ client/src/configs/firebaseApp.js
@@ -13,6 +13,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
 
-export default app
+export default firebaseApp
--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { FacebookAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import firebaseApp from "../../configs/firebaseApp";
+import { useNavigate } from "react-router-dom";
+import useSession from "../../hooks/useSession";
 
 export default function Login() {
-  return (
-    <form className='tw-fixed tw-top-0 tw-bottom-0 tw-right-0 tw-left-0 tw-flex tw-justify-center tw-items-center'>
-        <button className='btn btn-primary'>
-            Login with Facebook
-        </button>
-    </form>
-  )
+    const { user,loading } = useSession();
+    console.log("user", user);
+    const navigate = useNavigate();
+    const onSumit = (e) => {
+        e.preventDefault();
+        console.log("Login");
+        const provider = new FacebookAuthProvider();
+        const auth = getAuth(firebaseApp);
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                navigate("/");
+            })
+            .catch((error) => {
+                // error hadler
+                console.log("error");
+            });
+    };
+
+    useEffect(() => {
+      if(!loading) {
+        if (user) {
+            navigate("/");
+        }
+      }
+    }, [loading])
+    
+
+    return (
+        <form
+            onSubmit={onSumit}
+            className="tw-fixed tw-top-0 tw-bottom-0 tw-right-0 tw-left-0 tw-flex tw-justify-center tw-items-center"
+        >
+            <button className="btn btn-primary">Login with Facebook</button>
+        </form>
+    );
 }
