31b5e3f8cf8852c00459f7b6b2e40a5418fb717d
Teerut Srithongdee <>
2023-01-27 15:04:54 +0700

    add react router

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Home from "./pages/home/Home";
+import Trip from "./pages/trip/Trip";
+import CreateTrip from "./pages/trip/CreateTrip";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/trip",
+        element: <Trip />,
+    },
+    {
+        path: "/create-trip",
+        element: <CreateTrip />,
+    },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
